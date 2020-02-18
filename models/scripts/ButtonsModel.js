92332afd81eee2294b60d5ea259f5ae7e8193263
backspaces <owen@backspaces.net>
2020-02-18 13:23:13 -0700

    Models2: Add ButtonsView.js - Visualize ButtonsModel:   https://backspaces.github.io/agentscript/models2/?buttons - models2/ButtonsView: the new view for ButtonsModel - models/ButtonsModel:   - Simplify, and make largest cluster a model "goesouta" property - test/models.js:   - Update to current ButtonsModel properties

--- models/scripts/ButtonsModel.js
+++ models/scripts/ButtonsModel.js
@@ -15,22 +15,10 @@ class ButtonsModel extends Model {
     }
 
     setup() {
-        // this.refreshPatches = false
-
-        // this.turtles.setDefault('shape', 'circle')
-        // this.turtles.setDefault('size', 1.5)
         this.turtles.setDefault('heading', 0) // override promotion to random angle
 
-        // const population = 200 // number of buttons
-        this.clusterSize = 0
+        this.cluster = new Set()
         this.done = false
-        this.newCluster = false
-
-        // const cmap = ColorMap.grayColorMap(0, 100)
-        // const cmap = ColorMap.grayColorMap(150, 200)
-        // this.patches.ask(p => {
-        //     p.setColor(cmap.randomColor())
-        // })
 
         this.turtles.create(this.population, t =>
             t.setxy(...this.world.randomPatchPoint())
@@ -46,15 +34,11 @@ class ButtonsModel extends Model {
         this.links.create(b1, b2)
 
         const vertices = this.graphOf(b1)
-        for (const v of vertices) v.color = b1.color
+        if (vertices.size > this.cluster.size) this.cluster = vertices
 
-        this.newCluster = vertices.size > this.clusterSize
-        if (this.newCluster) this.clusterSize = vertices.size
-
-        if (this.clusterSize === this.turtles.length) {
-            // this.anim.setRate(30)
-            // console.log('done')
+        if (this.cluster.size === this.turtles.length) {
             this.done = true
+            console.log(`Done at tick: ${this.ticks}`)
         }
     }
 
---
+++ models2/ButtonsView.js
@@ -0,0 +1,40 @@
+import ColorMap from '../src/ColorMap.js'
+import TwoView from '../src/TwoView.js'
+
+const patchesColors = ColorMap.DarkGray
+const turtleColors = ColorMap.Basic16
+const linkColor = 'rgba(255, 255, 255, 0.50'
+const shape = 'circle'
+const shapeSize = 2
+const clusterColor = 'red'
+
+const viewOptions = { patchSize: 20 }
+
+function newView(model, options = {}) {
+    const view = new TwoView(model.world, Object.assign(viewOptions, options))
+    // one-time initialization: Create static patch colors
+    view.createPatchPixels(i => patchesColors.randomColor().pixel)
+    return view
+}
+
+function drawView(model, view) {
+    // view.clear()
+    view.drawPatches() // redraw cached patches colors
+
+    view.drawLinks(model.links, { color: linkColor, width: 1 })
+
+    // Draw all the turtles a constant color (from t.id)
+    view.drawTurtles(model.turtles, t => ({
+        shape: shape,
+        color: turtleColors.atIndex(t.id).css,
+        size: shapeSize,
+    }))
+    // Now recolor the current cluster
+    view.drawTurtles(Array.from(model.cluster), t => ({
+        shape: shape,
+        color: clusterColor,
+        size: shapeSize,
+    }))
+}
+
+export default { newView, drawView }
