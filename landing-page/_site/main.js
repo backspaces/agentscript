65289d7ad0266d83663534c634b8eb92455b45a8
Benny Lichtner <bennlich@gmail.com>
2021-03-02 12:28:06 -0800

    landing-page: delete _site

--- landing-page/_site/main.js
+++
@@ -1,6 +0,0 @@
-import './src/tutorial.js'
-import './src/landing-page.js'
-
-document.querySelector('[learn-more-button]').addEventListener('click', () => {
-  document.querySelector('[learn-more-button-target]').scrollIntoView({ behavior: 'smooth' })
-})
