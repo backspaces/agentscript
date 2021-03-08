47e5fcb995ac43a2a64ea9d5e63dd4ac06d8dabc
Benny Lichtner <bennlich@gmail.com>
2021-03-07 23:17:45 -0800

    landing-page: be explicit about our template types

--- landing-page-src/.eleventy.js
+++ landing-page-src/.eleventy.js
@@ -14,6 +14,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("main.js")
 
   return {
+    templateFormats: ["html"],
     dir: {
       output: "./dev-build"
     }
