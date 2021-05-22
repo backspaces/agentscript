92a2253df3558a5ecbc06d37ab28e2dbb046e043
backspaces <owen@backspaces.net>
2021-05-22 14:32:33 -0600

    Deno running Models, IDE/AS interop - IDE/AS interop:   - Partial imports via named imports should not be used.     - This allows the bundle usage to be identical to the module use.   - Fixed in DropletsModel.js - tests/deno0.js   - Uses HelloWorld imports and runs in deno. How cool is that!

--- models/scripts/DropletsModel.js
+++ models/scripts/DropletsModel.js
@@ -8,8 +8,7 @@ var Model = AS.Model
 //   mapzenDataSet
 //   mapboxDataSet
 // Use mapzenDataSet for free amazon elevation, redfishUSDataSet for high rez
-import { mapzenDataSet } from '../src/TileDataSet.js'
-// const { mapzenDataSet, redfishUSDataSet } = TileDataSet
+var TileDataSet = AS.TileDataSet
 
 class DropletsModel extends Model {
     zxy = [13, 1594, 3339]
@@ -34,7 +33,7 @@ class DropletsModel extends Model {
     }
 
     async startup() {
-        const elevation = await mapzenDataSet(...this.zxy)
+        const elevation = await TileDataSet.mapzenDataSet(...this.zxy)
         this.installDataSets(elevation)
     }
     installDataSets(elevation) {
