ae92ea7256e3897cb6a9a9be822514d2595cf359
Owen Densmore <owen@backspaces.net>
2022-02-26 15:38:58 -0700

    cleanup: removed more unused dev dependencies - removed from package.json:   "esbuild": "^0.12.15",   "esinstall": "^1.1.7", - puppeteer: nailed version, avoid odd behaviors in test/models.js   "puppeteer": "^13.0.0",

--- test/models.js
+++ test/models.js
@@ -30,7 +30,7 @@ let models = shell
     .split('\n')
     .map(str => str.charAt(0).toLowerCase() + str.slice(1))
 
-if (debug) models = ['droplets', 'mouse'] // debug. Also headless below
+// if (debug) models = ['droplets', 'mouse'] // debug. Also headless below
 // models = ['droplets'] // debug. Also headless below
 // models = models.filter(m => m !== 'droplets')
 // models = models.filter(m => !['droplets', 'gridPath'].includes(m))
