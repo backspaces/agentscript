8e999fe5f2e6af122c35df2fcaa43499c2e8d3aa
Owen Densmore <owen@backspaces.net>
2022-07-21 12:27:56 -0600

    Revert back to Model === Model3D after passing prior test

--- src/Model.js
+++ src/Model.js
@@ -1,61 +1,40 @@
-// import Model from './Model3D.js'
-// /** @class */
-
-// /**
-//  * This is our default Model which simply re-exports Model3D
-//  * Thus when using:
-//  *
-//  * import Model from './Model.js'
-//  *
-//  * ..you will be using ./Model3D
-//  *
-//  * @param {Object|World} [worldOptions=World.defaultOptions()]
-//  *  Identical to {@link Model3D}
-//  */
-
-// export default Model
-
-// ======================================
-
-import Turtle3D from './Turtle3D.js'
-import Model2D from './Model2D.js'
+import Model from './Model3D.js'
 /** @class */
 
-class Model extends Model2D {
-    use3D() {
-        return !(this.world.minZ === 0 && this.world.maxZ === 0)
-    }
-    initAgentSet(name, AgentsetClass, AgentClass) {
-        if (name === 'turtles' && this.use3D()) AgentClass = Turtle3D
-        super.initAgentSet(name, AgentsetClass, AgentClass)
-    }
-}
+/**
+ * This is our default Model which simply re-exports Model3D
+ * Thus when using:
+ *
+ * import Model from './Model.js'
+ *
+ * ..you will be using ./Model3D
+ *
+ * @param {Object|World} [worldOptions=World.defaultOptions()]
+ *  Identical to {@link Model3D}
+ */
 
 export default Model
 
 // ======================================
 
-// Model3D:
+// This works! But primarily for GeoWorld models.
+// And would fail to catch standard 2.5D models which are fine
+// with Model2D which accepts z values.
+
 // import Turtle3D from './Turtle3D.js'
 // import Model2D from './Model2D.js'
+// /** @class */
 
-// /**
-//  * Model3D uses Model2D with one change:
-//  *
-//  * - model.Turtles: an array {@link Turtles} of {@link Turtle3D} instances
-//  *
-//  * Model3D is our default Model.
-//  *
-//  * @param {Object|World} [worldOptions=World.defaultOptions()]
-//  *
-//  */
-// class Model3D extends Model2D {
+// class Model extends Model2D {
+//     use3D() {
+//         return !(this.world.minZ === 0 && this.world.maxZ === 0)
+//     }
 //     initAgentSet(name, AgentsetClass, AgentClass) {
-//         if (name === 'turtles') AgentClass = Turtle3D
+//         if (name === 'turtles' && this.use3D()) AgentClass = Turtle3D
 //         super.initAgentSet(name, AgentsetClass, AgentClass)
 //     }
 // }
 
-// export default Model3D
+// export default Model
 
 // ======================================
