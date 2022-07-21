c3c3a9c281c9fed78276ae97db82229b2966381a
Owen Densmore <owen@backspaces.net>
2022-07-21 11:59:56 -0600

    Testing: Model chooses 2D/3D based on min/max Z =-- 0

--- src/Model.js
+++ src/Model.js
@@ -1,48 +1,61 @@
-import Model from './Model3D.js'
+// import Model from './Model3D.js'
+// /** @class */
+
+// /**
+//  * This is our default Model which simply re-exports Model3D
+//  * Thus when using:
+//  *
+//  * import Model from './Model.js'
+//  *
+//  * ..you will be using ./Model3D
+//  *
+//  * @param {Object|World} [worldOptions=World.defaultOptions()]
+//  *  Identical to {@link Model3D}
+//  */
+
+// export default Model
+
+// ======================================
+
+import Turtle3D from './Turtle3D.js'
+import Model2D from './Model2D.js'
 /** @class */
 
-/**
- * This is our default Model which simply re-exports Model3D
- * Thus when using:
- *
- * import Model from './Model.js'
- *
- * ..you will be using ./Model3D
- *
- * @param {Object|World} [worldOptions=World.defaultOptions()]
- *  Identical to {@link Model3D}
- */
+class Model extends Model2D {
+    use3D() {
+        return !(this.world.minZ === 0 && this.world.maxZ === 0)
+    }
+    initAgentSet(name, AgentsetClass, AgentClass) {
+        if (name === 'turtles' && this.use3D()) AgentClass = Turtle3D
+        super.initAgentSet(name, AgentsetClass, AgentClass)
+    }
+}
 
 export default Model
 
 // ======================================
 
-// import Model2D from '../src/Model2D.js'
-// import Turtle3D from '../src/Turtle3D'
-// /** @class */
+// Model3D:
+// import Turtle3D from './Turtle3D.js'
+// import Model2D from './Model2D.js'
 
-// class Model extends Model2D {
-//     use3D() {
-//         return !(this.world.minZ === 0 && this.world.maxZ === 0)
-//     }
+// /**
+//  * Model3D uses Model2D with one change:
+//  *
+//  * - model.Turtles: an array {@link Turtles} of {@link Turtle3D} instances
+//  *
+//  * Model3D is our default Model.
+//  *
+//  * @param {Object|World} [worldOptions=World.defaultOptions()]
+//  *
+//  */
+// class Model3D extends Model2D {
 //     initAgentSet(name, AgentsetClass, AgentClass) {
-//         if (name === 'turtles' && this.use3D()) AgentClass = Turtle3D
+//         if (name === 'turtles') AgentClass = Turtle3D
 //         super.initAgentSet(name, AgentsetClass, AgentClass)
 //     }
 // }
 
-// export default Model
+// export default Model3D
 
 // ======================================
-// Try AsyncFunction?? top level await?
-// class Model extends Model2D {
-//     use3D() {
-//         return !(this.world.minZ === 0 && this.world.maxZ === 0)
-//     }
-//     initAgentSet(name, AgentsetClass, AgentClass) {
-//         if (name === 'turtles' && this.use3D()) AgentClass = Turtle3D
-//         super.initAgentSet(name, AgentsetClass, AgentClass)
-//     }
-// }
-
-// export default Model
