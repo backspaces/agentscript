98267c02b68e9e29146b5c1ad60d14ca666431ff
Joshua Thorp <joshua@stigmergic.net>
2023-03-16 16:56:01 -0600

    population is not wired up

--- models/MouseModel.js
+++ models/MouseModel.js
@@ -12,7 +12,7 @@ export default class MouseModel extends Model {
     setup() {
         const { patches, turtles, links } = this
         // this is sorta slick NL use. Can easily be two liner.
-        patches.nOf(20).ask(p => p.sprout(1))
+        patches.nOf(this.population).ask(p => p.sprout(1))
 
         // again, kinda slick, can be broken into a couple of lines.
         turtles.ask(t => links.create(t, turtles.otherOneOf(t)))
