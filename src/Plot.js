df5af024cc447ac79c57e27a262143f0b84e05f5
Owen Densmore <owen@backspaces.net>
2023-09-15 15:09:57 -0600

    Plot.js, tests/plots - src/Plot.js   - Add linePlot & scatterPlot - tests/plots/   - Add two examples: lines.html & scatter.html   - Add a testing example: tests.html

--- src/Plot.js
+++ src/Plot.js
@@ -12,9 +12,15 @@ class Plot {
             title: 'Data',
             width: 600,
             height: 300,
+
+            cursor: { show: true },
+            legend: { show: true },
+
             xRange: null, // static x values, use [0,100] format
             yRange: null, // ditto for y
             xIsTime: false,
+            xAxisLabel: 'x',
+
             series: [
                 {
                     label: 'x', // in-legend display
@@ -27,12 +33,13 @@ class Plot {
                 //     dash: [10, 5],
                 //     points: { space: 0 },
                 //     paths: u => null,
+                //     paths: uPlot.paths.bars({ size: [1] }),
                 //     band: false, // for two series to create highlow-bands
                 // },
             ],
             scales: {
                 x: {
-                    time: false, // don't use time x vals, use x ints
+                    time: false, // don't use time x vals, use x numbers
                     // range: [0, 100], // limit x data location
                 },
                 y: {
@@ -52,40 +59,149 @@ class Plot {
     // },
     constructor(div, pens, options = {}) {
         options = Object.assign(Plot.defaultOptions(), options)
+        if (util.isString(div)) div = document.getElementById(div)
+
         options.scales.x.time = options.xIsTime
+        options.series[0].label = options.xAxisLabel
 
         if (options.xRange) options.scales.x.range = options.xRange
         if (options.yRange) options.scales.y.range = options.yRange
 
-        const data = [[]]
-        const dataArrays = {}
+        // const data = [[]]
+        // const dataArrays = {}
 
+        // Use pens object to setup colors and shapes
         util.forLoop(pens, (val, key) => {
             // complex pens are series objects w/ label set to key
             if (util.isObject(val)) {
+                // if val is object, the object is a user supplied options object
                 val.label = key
                 options.series.push(val)
+            } else if (util.isArray(val)) {
+                // if val is an array, deconstruct to a color & pen
+                const [color, penTypeName] = val
+                const penType = this.penType(penTypeName)
+                penType.label = key
+                penType.stroke = color
+                options.series.push(penType)
             } else {
+                // otherwise push an object with label = key, stroke = val
                 options.series.push({
                     label: key,
-                    stroke: Array.isArray(val) ? val[0] : val,
-                    fill: Array.isArray(val) ? val[1] : null,
-                    // points: { space: 0.1 },
+                    stroke: val,
                 })
             }
-            data.push([])
-            dataArrays[key] = data.at(-1)
+
+            // // data is an array of 1 + num pens empty arrays, 1 for xs + num ys
+            // data.push([])
+            // // dataArrays have num key/val pairs.
+            // // NOTE: key = the pen key, val is the associated data array above.
+            // // Adding to dataArrays arrays actually adds to the data!
+            // dataArrays[key] = data.at(-1) // -1 returns the last array in the data arrays
         })
 
-        if (util.isString(div)) div = document.getElementById(div)
+        const [data, dataArrays] = this.createDataObject(pens)
         let uplot = new uPlot(options, data, div)
         Object.assign(this, { div, options, data, dataArrays, uplot })
+
+        util.toWindow({ options, data, dataArrays })
     }
 
-    get numPens() {
+    createDataObject(pens) {
+        const data = [[]]
+        const dataArrays = {}
+        util.forLoop(pens, (val, key) => {
+            // data is an array of 1 + num pens empty arrays, 1 for xs + num ys
+            data.push([])
+            // dataArrays have num key/val pairs.
+            // NOTE: key = the pen key, val is the associated data array above.
+            // Adding to dataArrays arrays actually adds to the data!
+            dataArrays[key] = data.at(-1) // -1 returns the last array in the data arrays
+        })
+        return [data, dataArrays]
+
+        // data.push([])
+        // // dataArrays have num key/val pairs.
+        // // NOTE: key = the pen key, val is the associated data array above.
+        // // Adding to dataArrays arrays actually adds to the data!
+        // dataArrays[key] = data.at(-1) // -1 returns the last array in the data arrays
+    }
+
+    numPens() {
         return this.data.length - 1
     }
+    penType(type) {
+        switch (type) {
+            case 'lines':
+                return { width: 1 }
+            case 'thickLines':
+                return { width: 2 }
+            case 'dashedLines':
+                return { dash: [10, 5] }
+            case 'thickDashedLines':
+                return { dash: [10, 5], width: 2 }
+            case 'points':
+                return {
+                    points: { space: 0 },
+                    paths: u => null,
+                }
+            case 'thickPoints':
+                return {
+                    width: 2,
+                    points: { space: 0 },
+                    paths: u => null, // no linesd between points
+                }
+            case 'bars': {
+                return {
+                    fill: 'rgba(255, 0, 0, 0.3)',
+                    points: { space: 0 },
+                    paths: uPlot.paths.bars({
+                        size: [1],
+                    }),
+                }
+            }
+        }
+        throw Error('penType: unknown type.')
+    }
+    penTypes() {
+        return [
+            lines,
+            thickLines,
+            dashedLines,
+            thickDashedLines,
+            pointsthickPoints,
+            bars,
+        ]
+    }
 
+    scatterPlot(pens) {
+        util.forLoop(pens, (val, key) => {
+            // each val is an array of {x,y} objects. convert to array
+            val = util.sortObjs(val, 'x')
+            const xs = val.map(o => o.x)
+            const ys = val.map(o => o.y)
+
+            this.data[0] = xs
+            this.data[1] = ys
+
+            this.drawData()
+        })
+
+        // const xys = util.sortObjs(arrayOfXYs, 'x')
+        // const xs = xys.map(o => o.x)
+        // const ys = xys.map(o => o.y)
+        // this.data[0] = xs
+        // this.data[1] = ys
+        // this.drawData()
+    }
+    linePlot(pens, draw = true) {
+        const xs = this.data[0]
+        xs.push(xs.length)
+        util.forLoop(pens, (val, key) => {
+            this.dataArrays[key].push(val)
+        })
+        if (draw) this.drawData()
+    }
     // points: like pens but the next number to be plotted
     // points: {
     //     infected: 42, // often like: infected: model.infected
@@ -93,18 +209,34 @@ class Plot {
     //     resistant: 23,
     // },
     // You can just reuse pens obj with data not colors
-    setData(points, draw = true) {
-        const xs = this.data[0]
-        xs.push(xs.length)
+    // setData(points, draw = true) {
+    //     if (util.isArray(points)) {
+    //         this.data[0] = points[0]
+    //         this.data[1] = points[1]
+    //     } else {
+    //         // for lines:
+    //         const xs = this.data[0]
+    //         xs.push(xs.length)
+    //         // console.log('xs:', xs)
+    //     }
 
-        util.forLoop(points, (val, key) => {
-            this.dataArrays[key].push(val)
-        })
+    //     if (!util.isArray(points)) {
+    //         util.forLoop(points, (val, key) => {
+    //             if (util.isNumber(val)) {
+    //                 this.dataArrays[key].push(val)
+    //             } else if (util.isArray(val)) {
+    //                 this.dataArrays[key] = val
+    //             } else {
+    //                 console.log('Oops!')
+    //             }
+    //         })
+    //     }
 
-        // If draw is false, don't draw, just update this.data
-        // This allows updating every n steps, or until the model is done.
-        if (draw) this.drawData()
-    }
+    //     // If draw is false, don't draw, just update this.data
+    //     // This allows updating every n steps, or until the model is done.
+    //     if (draw) this.drawData()
+    // }
+
     drawData() {
         this.uplot.setData(this.data)
     }
