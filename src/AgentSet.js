67ce4ee07be185ffe8aeac6d830d802d5598b15d
Owen Densmore <owen@backspaces.net>
2023-10-04 11:15:22 -0600

    AgentSet: cleanup, remove unused askXX methods, not used.

--- src/AgentSet.js
+++ src/AgentSet.js
@@ -104,6 +104,74 @@ class AgentSet extends AgentArray {
     }
 
     /**
+     * @param {Object} o An Agent to be added to this AgentSet
+     * @returns {Object} The input Agent, bound to this AgentSet.
+     * @description
+     * Add an Agent to this AgentSet.  Only used by factory methods.
+     * Adds the `id` property to Agent. Increment AgentSet `ID`.
+     */
+    addAgent(o = undefined) {
+        // o only for breeds adding themselves to their baseSet
+        o = o || Object.create(this.agentProto) // REMIND: Simplify! Too slick.
+        if (this.isBreedSet()) {
+            this.baseSet.addAgent(o)
+        } else {
+            o.id = this.ID++
+            if (o.agentConstructor) o.agentConstructor()
+        }
+        this.push(o)
+        return o
+    }
+    /**
+     * Remove an Agent from this AgentSet
+     *
+     * @param {Object} o The Agent to be removed
+     * @returns {AgentSet} This AgentSet with the Agent removed
+     */
+    removeAgent(o) {
+        // Note removeAgent(agent) different than remove(agent) which
+        // simply removes the agent from it's array
+
+        if (o.id != -1) {
+            // Remove me from my baseSet
+            if (this.isBreedSet()) this.baseSet.remove(o, 'id')
+            // Remove me from my set.
+            this.remove(o, 'id')
+        }
+
+        return this
+    }
+    /**
+     * Move an agent from its AgentSet/breed to be in this AgentSet/breed
+     *
+     * @param {Agent} a An agent, a member of another AgentSet
+     * @returns {Agent} The updated agent
+     */
+    setBreed(a) {
+        // change agent a to be in this breed
+        // Return if `a` is already of my breed
+        if (a.agentSet === this) return
+        // Remove/insert breeds (not baseSets) from their agentsets
+        if (a.agentSet.isBreedSet()) a.agentSet.remove(a, 'id')
+        if (this.isBreedSet()) this.insert(a, 'id')
+
+        // Make list of `a`'s vars and my ownvars.
+        const avars = a.agentSet.ownVariables
+        // First remove `a`'s vars not in my ownVariables
+        for (const avar of avars) {
+            if (!this.ownVariables.includes(avar)) delete a[avar]
+        }
+        // Now add ownVariables to `a`'s vars, default to 0.
+        // If ownvar already in avars, it is not modified.
+        for (const ownvar of this.ownVariables) {
+            if (!avars.includes(ownvar)) a[ownvar] = 0
+        } // NOTE: NL uses 0, maybe we should use null?
+
+        // Give `a` my defaults/statics
+        return Object.setPrototypeOf(a, this.agentProto)
+    }
+
+    /**
      * Create a subarray of this AgentSet.
      * Example: create a people breed of Turtles:
      *
@@ -147,50 +215,12 @@ class AgentSet extends AgentArray {
     }
 
     /**
-     * @param {Object} o An Agent to be added to this AgentSet
-     * @returns {Object} The input Agent, bound to this AgentSet.
-     * @description
-     * Add an Agent to this AgentSet.  Only used by factory methods.
-     * Adds the `id` property to Agent. Increment AgentSet `ID`.
-     */
-    addAgent(o = undefined) {
-        // o only for breeds adding themselves to their baseSet
-        o = o || Object.create(this.agentProto) // REMIND: Simplify! Too slick.
-        if (this.isBreedSet()) {
-            this.baseSet.addAgent(o)
-        } else {
-            o.id = this.ID++
-            if (o.agentConstructor) o.agentConstructor()
-        }
-        this.push(o)
-        return o
-    }
-    /**
      * Remove all Agents from this AgentSet using agent.die() for each agent.
      *
      */
     clear() {
         // die() is an agent method. sets it's id to -1
         while (!this.isEmpty()) this.last().die()
-    }
-    /**
-     * Remove an Agent from this AgentSet
-     *
-     * @param {Object} o The Agent to be removed
-     * @returns {AgentSet} This AgentSet with the Agent removed
-     */
-    removeAgent(o) {
-        // Note removeAgent(agent) different than remove(agent) which
-        // simply removes the agent from it's array
-
-        if (o.id != -1) {
-            // Remove me from my baseSet
-            if (this.isBreedSet()) this.baseSet.remove(o, 'id')
-            // Remove me from my set.
-            this.remove(o, 'id')
-        }
-
-        return this
     }
 
     /**
@@ -213,51 +243,6 @@ class AgentSet extends AgentArray {
     getDefault(name) {
         return this.agentProto[name]
     }
-    // Used when getter/setter's need to know if get/set default
-    // settingDefault(agent) {
-    //     return agent.id == null
-    // }
-
-    // Declare variables of an agent class. May deprecate if not needed.
-    // `varnames` is a string of space separated names
-    // own(varnames) {
-    //     // if (this.isBreedSet())
-    //     //   this.ownVariables = util.clone(this.baseSet.ownVariables)
-    //     for (const name of varnames.split(' ')) {
-    //         this.setDefault(name, null)
-    //         this.ownVariables.push(name)
-    //     }
-    // }
-
-    /**
-     * Move an agent from its AgentSet/breed to be in this AgentSet/breed
-     *
-     * @param {Agent} a An agent, a member of another AgentSet
-     * @returns {Agent} The updated agent
-     */
-    setBreed(a) {
-        // change agent a to be in this breed
-        // Return if `a` is already of my breed
-        if (a.agentSet === this) return
-        // Remove/insert breeds (not baseSets) from their agentsets
-        if (a.agentSet.isBreedSet()) a.agentSet.remove(a, 'id')
-        if (this.isBreedSet()) this.insert(a, 'id')
-
-        // Make list of `a`'s vars and my ownvars.
-        const avars = a.agentSet.ownVariables
-        // First remove `a`'s vars not in my ownVariables
-        for (const avar of avars) {
-            if (!this.ownVariables.includes(avar)) delete a[avar]
-        }
-        // Now add ownVariables to `a`'s vars, default to 0.
-        // If ownvar already in avars, it is not modified.
-        for (const ownvar of this.ownVariables) {
-            if (!avars.includes(ownvar)) a[ownvar] = 0
-        } // NOTE: NL uses 0, maybe we should use null?
-
-        // Give `a` my defaults/statics
-        return Object.setPrototypeOf(a, this.agentProto)
-    }
 
     /**
      * Call fcn(agent, index, array) for each item in AgentArray.
@@ -275,62 +260,62 @@ class AgentSet extends AgentArray {
         }
     }
 
-    /**
-     * A much stronger version of ask(fcn) with stronger mutability guards.
-     *
-     * @param {Function} fcn fcn(agent, index?, array?)
-     */
-    askSet(fcn) {
-        // Manages immutability reasonably well.
-        if (this.length === 0) return
-        // Patches are static
-        if (this.name === 'patches') super.forLoop(fcn)
-        else if (this.isBaseSet()) this.baseSetAsk(fcn)
-        else if (this.isBreedSet()) this.cloneAsk(fcn)
-    }
+    // /**
+    //  * A much stronger version of ask(fcn) with stronger mutability guards.
+    //  *
+    //  * @param {Function} fcn fcn(agent, index?, array?)
+    //  */
+    // askSet(fcn) {
+    //     // Manages immutability reasonably well.
+    //     if (this.length === 0) return
+    //     // Patches are static
+    //     if (this.name === 'patches') super.forLoop(fcn)
+    //     else if (this.isBaseSet()) this.baseSetAsk(fcn)
+    //     else if (this.isBreedSet()) this.cloneAsk(fcn)
+    // }
 
-    // An ask function for mutable baseSets.
-    // BaseSets can only add past the end of the array.
-    // This allows us to manage mutations by allowing length change,
-    // and managing deletions only within the original length.
-    baseSetAsk(fcn) {
-        if (this.length === 0) return
-        const lastID = this.last().id
+    // // An ask function for mutable baseSets.
+    // // BaseSets can only add past the end of the array.
+    // // This allows us to manage mutations by allowing length change,
+    // // and managing deletions only within the original length.
+    // baseSetAsk(fcn) {
+    //     if (this.length === 0) return
+    //     const lastID = this.last().id
 
-        // Added obj's have id > lastID. Just check for deletions.
-        // There Be Dragons:
-        // - AgentSet can become length 0 if all deleted
-        // - For loop tricky:
-        //   - i can become negative w/in loop:
-        //   - i can become bigger than current AgentSet
-        //   - Guard w/ i<len & i>=0
-        for (let i = 0; i < this.length; i++) {
-            const obj = this[i]
-            const id = obj.id
-            if (id > lastID) break
-            fcn(obj, i, this)
-            if (i >= this.length) break
-            if (this[i].id > id) {
-                while (i >= 0 && this[i].id > id) i-- // ok if -1
-            }
-        }
-    }
+    //     // Added obj's have id > lastID. Just check for deletions.
+    //     // There Be Dragons:
+    //     // - AgentSet can become length 0 if all deleted
+    //     // - For loop tricky:
+    //     //   - i can become negative w/in loop:
+    //     //   - i can become bigger than current AgentSet
+    //     //   - Guard w/ i<len & i>=0
+    //     for (let i = 0; i < this.length; i++) {
+    //         const obj = this[i]
+    //         const id = obj.id
+    //         if (id > lastID) break
+    //         fcn(obj, i, this)
+    //         if (i >= this.length) break
+    //         if (this[i].id > id) {
+    //             while (i >= 0 && this[i].id > id) i-- // ok if -1
+    //         }
+    //     }
+    // }
 
-    // For breeds, mutations can occur in many ways.
-    // This solves this by cloning the initial array and
-    // managing agents that have died or changed breed.
-    // In other words, we can be concerned only with mutations
-    // of the agents themselves.
-    cloneAsk(fcn) {
-        const clone = this.clone()
-        for (let i = 0; i < clone.length; i++) {
-            const obj = clone[i]
-            // obj.id > 0: obj.die() sets id to -1
-            if (obj.breed == this && obj.id > 0) {
-                fcn(obj, i, clone)
-            }
-        }
-    }
+    // // For breeds, mutations can occur in many ways.
+    // // This solves this by cloning the initial array and
+    // // managing agents that have died or changed breed.
+    // // In other words, we can be concerned only with mutations
+    // // of the agents themselves.
+    // cloneAsk(fcn) {
+    //     const clone = this.clone()
+    //     for (let i = 0; i < clone.length; i++) {
+    //         const obj = clone[i]
+    //         // obj.id > 0: obj.die() sets id to -1
+    //         if (obj.breed == this && obj.id > 0) {
+    //             fcn(obj, i, clone)
+    //         }
+    //     }
+    // }
 }
 
 export default AgentSet
