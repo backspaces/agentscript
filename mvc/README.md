9f620679410885bcfa10e0c0c8feca05845869ba
Owen Densmore <owen@backspaces.net>
2024-02-29 11:42:29 -0700

    helloKeys, helloKeys, Keyboard - update mvc/ control samples   mvc/helloKeys.html mvc/helloKeys.htm - vastly simplify src/Keyboard.js   - remove use of modifiers, use the char they generate     ex: opt-a = å, opt-g = ©     ditto for shifted keys: shift , = <

--- mvc/README.md
+++ mvc/README.md
@@ -5,6 +5,7 @@ Code    | Run demo
 [flock](https://github.com/backspaces/agentscript/tree/master/mvc/flock.html#L1) | [https://code.agentscript.org/mvc/flock.html](https://code.agentscript.org/mvc/flock.html)
 [gui](https://github.com/backspaces/agentscript/tree/master/mvc/gui.html#L1) | [https://code.agentscript.org/mvc/gui.html](https://code.agentscript.org/mvc/gui.html)
 [helloGUI](https://github.com/backspaces/agentscript/tree/master/mvc/helloGUI.html#L1) | [https://code.agentscript.org/mvc/helloGUI.html](https://code.agentscript.org/mvc/helloGUI.html)
+[helloKeys](https://github.com/backspaces/agentscript/tree/master/mvc/helloKeys.html#L1) | [https://code.agentscript.org/mvc/helloKeys.html](https://code.agentscript.org/mvc/helloKeys.html)
 [helloMouse](https://github.com/backspaces/agentscript/tree/master/mvc/helloMouse.html#L1) | [https://code.agentscript.org/mvc/helloMouse.html](https://code.agentscript.org/mvc/helloMouse.html)
 [mouse](https://github.com/backspaces/agentscript/tree/master/mvc/mouse.html#L1) | [https://code.agentscript.org/mvc/mouse.html](https://code.agentscript.org/mvc/mouse.html)
 [navigate3d](https://github.com/backspaces/agentscript/tree/master/mvc/navigate3d.html#L1) | [https://code.agentscript.org/mvc/navigate3d.html](https://code.agentscript.org/mvc/navigate3d.html)
--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,82 +1,52 @@
 class Keyboard {
     /**
      * Creates an instance of Keyboard.
-     * The objects in the keyModObjs look like:
+     * The objects in the keyTemplates look like:
      *
      * @constructor
      * @param {Object[]} Commands array of keyboard command objects
-     * @param {String} ModifierKey default modifier key(alt, shift, ..)
      *
      * @example
      * // A command object looks like:
      * {
      *     key: 's',
      *     cmd: () => anim.start(),
-     *     modifier: 'alt', // if messing, use the constructor's default modifier
      * }
      *
      *
      * @example
      * // The array of keyboard commands looks like:
-     *  const keyCommands = [
-     *      // These use the default modifier key, in this case the 'alt' key
+     *  const keyTemplates = [
      *      { key: 'q', cmd: () => console.log('q') },
      *      { key: '2', cmd: () => console.log('2') },
      *      { key: 'F2', cmd: () => console.log('F2') },
      *      { key: 'ArrowDown', cmd: () => console.log('ArrowDown') },
      *      { key: 'Escape', cmd: () => console.log('Escape') },
-     *      // Here we override the default modifier key
-     *      { modifier: 'shiftKey', key: 'a', cmd: () => console.log('shift A') },
-     *      { modifier: 'None', key: 'b', cmd: () => console.log('b') },
-     *      { modifier: '', key: 'c', cmd: () => console.log('c') }
      *  ]
-     *  const keyboard = new Keyboard(keyCommands, 'alt')
+     *  const keyboard = new Keyboard(keyTemplates)
      */
 
-    // default is no modifier
-    constructor(keyModObjs, defaultModifier) {
-        const modifiers = ['altKey', 'ctrlKey', 'metaKey', 'shiftKey']
-        const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    constructor(keyTemplates) {
+        // const modifiers = ['altKey', 'ctrlKey', 'metaKey', 'shiftKey']
 
-        // this.processAllKeys = keyModObjs.length = 0
-        // Use {key: 'all', cmd: xxx} to grab all events
-
-        const keymods = {}
-        keyModObjs.forEach(obj => {
-            let { modifier, key, cmd } = obj
+        const keyCommands = {}
+        keyTemplates.forEach(obj => {
+            // let { modifier, key, cmd } = obj
+            let { key, cmd } = obj
 
             if (!cmd) throw Error('No cmd given for ' + obj)
 
-            if (key.length === 1)
-                key = digits.includes(key)
-                    ? 'Digit' + key
-                    : 'Key' + key.toUpperCase()
+            // if (modifier && !modifiers.includes(modifier)) {
+            //     throw Error('Keyboard: ilegal modifier: ' + modifier)
+            // }
 
-            if (modifier === 'None') {
-                modifier = ''
-            } else if (modifier == undefined) {
-                if (['None', '', undefined, null].includes(defaultModifier)) {
-                    modifier = ''
-                } else {
-                    modifier = defaultModifier
-                    if (!modifier.endsWith('Key')) {
-                        modifier += 'Key'
-                    }
-                    if (!modifiers.includes(modifier)) {
-                        throw Error('Keyboard: ilegal modifier: ' + modifier)
-                    }
-                }
-            }
-
-            keymods[modifier + key] = { modifier, key, cmd }
+            keyCommands[key] = { key, cmd }
         })
 
-        console.log('keymods', keymods)
-        Object.assign(this, { modifiers, keymods })
+        // Object.assign(this, { modifiers, keyCommands })
+        Object.assign(this, { keyCommands })
+        console.log(this)
     }
-
-    // insure the cmd is executed in this name space:
-    handleEvent = ev => this.handleKeyboardEvent(ev)
 
     /**
      * Start the keyboard running
@@ -86,27 +56,6 @@ class Keyboard {
         return this
     }
 
-    handleKeyboardEvent(event) {
-        // https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event
-        if (event.isComposing || event.keyCode === 229 || event.repeat) return
-
-        const modifiers = this.modifiers.filter(modifier => event[modifier])
-        if (modifiers.length > 1)
-            throw Error('multiple modifier keys: ', modifiers)
-        const modifier = modifiers.length === 0 ? '' : modifiers[0]
-
-        console.log('e.key, e.code, modifier:', event.key, event.code, modifier)
-
-        const key = modifier + event.code
-        const keymod = this.keymods[key]
-        // console.log('key, keymod:', key, keymod)
-
-        if (keymod) {
-            // console.log(keymod.cmd)
-            keymod.cmd()
-        }
-        // else console.log(event)
-    }
     /**
      * Stop the keyboard running
      */
@@ -114,6 +63,25 @@ class Keyboard {
         // Note: multiple calls safe
         document.removeEventListener('keydown', this.handleEvent)
         return this
+    }
+
+    // insure the cmd is executed in this name space:
+    handleEvent = ev => this.handleKeyboardEvent(ev)
+
+    handleKeyboardEvent(event) {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event
+        if (event.isComposing || event.keyCode === 229 || event.repeat) {
+            console.log('skipping..', event)
+            return
+        }
+
+        console.log('event', event)
+        console.log('e.key, e.code:', event.key, event.code)
+
+        const key = this.keyCommands[event.key]
+        if (key) {
+            key.cmd()
+        }
     }
 }
 
