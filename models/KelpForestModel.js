a248366ac8ad58de5defac14b512828a4856222f
Owen Densmore <owen@backspaces.net>
2025-01-26 11:03:45 -0700

    Update KelpForestModel.js kelpforest.html

--- models/KelpForestModel.js
+++ models/KelpForestModel.js
@@ -1,15 +1,21 @@
 import * as util from 'https://code.agentscript.org/src/utils.js'
+import World from 'https://code.agentscript.org/src/World.js'
 import Model from 'https://code.agentscript.org/src/Model.js'
+
+// util.randomSeed() // causes each run to have same results for debugging
 
 class KelpForestModel extends Model {
     wiggleAngle = 45
     speed = 0.1
-    daysInYear = 364
-    // year = 0
 
     numKelp = 500
     numUrchin = 50
     numSeastar = 5
+
+    // worldOptions: patches -16, 16 or 33 x 33 with 0,0 origin
+    constructor(worldOptions = World.defaultOptions(16)) {
+        super(worldOptions)
+    }
 
     setup() {
         this.turtleBreeds('kelp urchin seaStar')
@@ -29,54 +35,20 @@ class KelpForestModel extends Model {
             const patch = this.patches.oneOf()
             t.setxy(patch.x, patch.y)
         })
-
-        // this.urchin.setDefault("atEdge", "bounce");
-
-        // this.urchin.speed = 0.1
-
-        // this.updateTicks = () => {
-        //     this.tickCounter++
-        //     if (this.tickCounter >= 366) {
-        //         this.tickCounter = 0
-        //     }
-        // }
-
-        // this.spawnUrchin = function spawnUrchin(tickCounter) {
-        //     if (tickCounter >= 365) {
-        //         this.urchin.create(this.urchin.length * 2, t => {
-        //             const patch = this.patches.oneOf()
-        //             t.setxy(patch.x, patch.y)
-        //         })
-        //     }
-        // }
-
-        // this.reseedKelp = function reseedKelp(tickCounter) {
-        //     if (tickCounter >= 365) {
-        //         this.kelp.create(this.kelp.length * 3, t => {
-        //             const patch = this.patches.oneOf()
-        //             t.setxy(patch.x, patch.y)
-        //         })
-        //     }
-        // }
-
-        // this.spawnSeaStars = function spawnSeaStars(tickCounter) {
-        //     if (this.seaStar.length > 0) {
-        //         if (tickCounter >= 365) {
-        //             this.seaStar.create(2, t => {
-        //                 const patch = this.patches.oneOf()
-        //                 t.setxy(patch.x, patch.y)
-        //             })
-        //         }
-        //     }
-        // }
     }
 
-    isNewYear() {
-        return this.ticks % this.daysInYear === this.daysInYear - 1
+    day() {
+        // days go from 0 to 364
+        return this.ticks % 365
+    }
+
+    year() {
+        // years start at 1
+        return Math.floor(this.ticks / 365) + 1
     }
 
     spawnUrchin() {
-        if (this.isNewYear()) {
+        if (this.day() === 0 && this.year() > 1) {
             this.urchin.create(this.urchin.length * 2, t => {
                 const patch = this.patches.oneOf()
                 t.setxy(patch.x, patch.y)
@@ -85,7 +57,7 @@ class KelpForestModel extends Model {
     }
 
     reseedKelp() {
-        if (this.isNewYear()) {
+        if (this.day() === 0 && this.year() > 1) {
             this.kelp.create(this.kelp.length * 3, t => {
                 const patch = this.patches.oneOf()
                 t.setxy(patch.x, patch.y)
@@ -94,8 +66,8 @@ class KelpForestModel extends Model {
     }
 
     spawnSeaStars() {
-        if (this.seaStar.length > 0) {
-            if (this.isNewYear()) {
+        if (this.day() === 0 && this.year() > 1) {
+            if (this.seaStar.length > 0) {
                 this.seaStar.create(2, t => {
                     const patch = this.patches.oneOf()
                     t.setxy(patch.x, patch.y)
@@ -104,45 +76,36 @@ class KelpForestModel extends Model {
         }
     }
 
-    step() {
-        // if (this.done) {
-        //     console.log('done, ticks =', this.ticks)
-        //     return
-        // }
+    closestNeighbor(turtle, neighbors) {
+        let closest = false
+        if (neighbors.length > 0) {
+            closest = neighbors.minOneOf(t => t.distance(turtle))
+        }
+        return closest
+    }
 
+    step() {
         this.urchin.ask(t => {
-            let closestSeaStar = false
-            if (this.seaStar.length > 0) {
-                closestSeaStar = this.seaStar.minOneOf(seaStar =>
-                    t.distance(seaStar)
-                )
-            }
+            const closestSeaStar = this.closestNeighbor(t, this.seaStar)
 
             if (closestSeaStar && t.distance(closestSeaStar <= 2)) {
                 const seaStarHeading = closestSeaStar.heading
                 t.heading = seaStarHeading * -1
-                t.forward(this.speed)
             } else {
                 t.heading += util.randomCentered(this.wiggleAngle)
-                t.forward(this.speed)
             }
+            t.forward(this.speed)
         })
 
         this.seaStar.ask(t => {
-            let closestUrchin = false
-            if (this.urchin.length > 0) {
-                closestUrchin = this.urchin.minOneOf(urchin =>
-                    t.distance(urchin)
-                )
-            }
+            const closestUrchin = this.closestNeighbor(t, this.urchin)
 
             if (closestUrchin && t.distance(closestUrchin) <= 2) {
                 t.face(closestUrchin)
-                t.forward(this.speed)
             } else {
                 t.heading += util.randomCentered(this.wiggleAngle)
-                t.forward(this.speed)
             }
+            t.forward(this.speed)
         })
 
         this.urchin.forEach(t => {
@@ -160,15 +123,7 @@ class KelpForestModel extends Model {
         this.spawnUrchin()
         this.reseedKelp()
         this.spawnSeaStars()
-
-        // this.done = this.ticks === this.daysInYear * 5
     }
-
-    // clearTurtles() {
-    //     this.urchin.clear()
-    //     this.kelp.clear()
-    //     this.seaStar.clear()
-    // }
 }
 
 export default KelpForestModel
